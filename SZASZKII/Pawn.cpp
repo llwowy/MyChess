e941345f2829deb86d99beb116b4f6446256e9d3
wluzp <wluzp@LAPTOP-6S71FRHE>
2023-06-07 22:12:44 +0200

    virtual setPiece

--- SZASZKII/Pawn.cpp
+++ SZASZKII/Pawn.cpp
@@ -12,18 +12,13 @@ Pawn::Pawn(const std::string& _id, std::vector<BoardTile*>& board) : Piece(_id) 
 	setTextureRect(sf::IntRect(0, 0, 16, 16));
 	setScale(7, 7);
 }
-void Pawn::Create_white_pawn() {
-	if (Piece_texture.loadFromFile("")) {
-		std::cout << "dupa";
-	}
-	setTexture(Piece_texture);
-}
+//void Pawn::Create_white_pawn() {
+//	if (Piece_texture.loadFromFile("")) {
+//		std::cout << "dupa";
+//	}
+//	setTexture(Piece_texture);
+//}
 
-void Pawn::set_Piece(const std::vector<BoardTile*> board, std::string Tile_id) {
-	auto it = std::find_if(board.begin(), board.end(), [Tile_id](BoardTile* Tile) {
-		return Tile->get_Tile_id() == Tile_id; });
-	setPosition((*it)->get_Tile_position());
-}
 
 
 
