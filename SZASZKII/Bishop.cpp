fb5829247fdd5c8dc834c209124d970a7315f444
LambuiB080 <lamblazej@gmail.com>
2023-06-10 00:32:43 +0200

    działająca kolizja gońca, co sprawia że goniec nigdy nie przeskoczy przez inną figurę

--- SZASZKII/Bishop.cpp
+++ SZASZKII/Bishop.cpp
@@ -22,7 +22,7 @@ void::Bishop::move(std::vector<BoardTile*>& board, const sf::Vector2i& mouse_pos
 				Tile->get_Tile_position().y <= mouse_position.y && Tile->get_Tile_position().y + Tile->get_Tile_size().y >= mouse_position.y); 
 			});
 
-		if (it != board.end() && (
+		if (it != board.end() && collider(_PawnsVec, (*it)->get_Tile_position()) && (
 			(*it)->get_Tile_position() == get_Starting_Piece_pos() + sf::Vector2f(1 * 112, 1 * 112) ||
 			(*it)->get_Tile_position() == get_Starting_Piece_pos() + sf::Vector2f(2 * 112, 2 * 112) ||
 			(*it)->get_Tile_position() == get_Starting_Piece_pos() + sf::Vector2f(3 * 112, 3 * 112) ||
@@ -60,7 +60,120 @@ void::Bishop::move(std::vector<BoardTile*>& board, const sf::Vector2i& mouse_pos
 	}
 }
 
+bool Bishop::collider(std::vector<Piece*> _PawnsVec, sf::Vector2f selected_Tile_pos) {
+	auto itr = std::find_if(_PawnsVec.begin(), _PawnsVec.end(), [selected_Tile_pos](Piece* _piece) {
+		return selected_Tile_pos == _piece->getPosition();
+		});
 
+	if (itr == _PawnsVec.end()) {
+		sf::Vector2f chosen_pos = selected_Tile_pos;
+		sf::Vector2f starting_pos = get_Starting_Piece_pos();
+		bool found_obstacle = false;
+
+		if (chosen_pos.x - starting_pos.x > 0 && chosen_pos.y - starting_pos.y > 0) {
+			for (int i = chosen_pos.x - starting_pos.x; i != 0;) {
+				itr = std::find_if(_PawnsVec.begin(), _PawnsVec.end(), [selected_Tile_pos, i](Piece* _piece) {
+					return selected_Tile_pos - sf::Vector2f(i,i) == _piece->getPosition();
+					});
+
+				if (itr != _PawnsVec.end()) {
+					found_obstacle = true;
+					return false;
+					break;
+				}
+				i -= 112;
+			}
+			if (found_obstacle == false) {
+				return true;
+			}
+		}
+		else if (chosen_pos.x - starting_pos.x > 0 && chosen_pos.y - starting_pos.y < 0) {
+			for (int i = chosen_pos.x - starting_pos.x; i != 0;) {
+				itr = std::find_if(_PawnsVec.begin(), _PawnsVec.end(), [selected_Tile_pos, i](Piece* _piece) {
+					return selected_Tile_pos - sf::Vector2f(i, -i) == _piece->getPosition();
+					});
+
+				if (itr != _PawnsVec.end()) {
+					found_obstacle = true;
+					return false;
+					break;
+				}
+				i -= 112;
+			}
+			if (found_obstacle == false) {
+				return true;
+			}
+		}
+		else if (chosen_pos.x - starting_pos.x < 0 && chosen_pos.y - starting_pos.y > 0) {
+			for (int i = chosen_pos.x - starting_pos.x; i != 0;) {
+				itr = std::find_if(_PawnsVec.begin(), _PawnsVec.end(), [selected_Tile_pos, i](Piece* _piece) {
+					return selected_Tile_pos - sf::Vector2f(i, -i) == _piece->getPosition();
+					});
+
+				if (itr != _PawnsVec.end()) {
+					found_obstacle = true;
+					return false;
+					break;
+				}
+				i += 112;
+			}
+			if (found_obstacle == false) {
+				return true;
+			}
+		}
+		else if (chosen_pos.x - starting_pos.x < 0 && chosen_pos.y - starting_pos.y < 0) {
+			for (int i = chosen_pos.x - starting_pos.x; i != 0;) {
+				itr = std::find_if(_PawnsVec.begin(), _PawnsVec.end(), [selected_Tile_pos, i](Piece* _piece) {
+					return selected_Tile_pos - sf::Vector2f(i, i) == _piece->getPosition();
+					});
+
+				if (itr != _PawnsVec.end()) {
+					found_obstacle = true;
+					return false;
+					break;
+				}
+				i += 112;
+			}
+			if (found_obstacle == false) {
+				return true;
+			}
+		}
+
+	}
+	else {
+		return false;
+	}
+}
+
+//bool Bishop::collider(std::vector<Piece*> _PawnsVec, sf::Vector2f selected_Tile_pos) {
+//	auto itr = std::find_if(_PawnsVec.begin(), _PawnsVec.end(), [selected_Tile_pos](Piece* _piece) {
+//		return selected_Tile_pos == _piece->getPosition();
+//		});
+//	if (itr == _PawnsVec.end()) {
+//		return true;
+//	}
+//	else {
+//		return false;
+//	}
+//}
+
+void Bishop::take(std::vector<BoardTile*>& board, const sf::Vector2i& mouse_position, std::vector<Piece*> _PawnsVec) {
+	if (get_is_selected()) {
+		auto it = std::find_if(board.begin(), board.end(), [mouse_position](BoardTile* Tile) {
+			return (Tile->get_Tile_position().x <= mouse_position.x && Tile->get_Tile_position().x + Tile->get_Tile_size().x >= mouse_position.x &&
+				Tile->get_Tile_position().y <= mouse_position.y && Tile->get_Tile_position().y + Tile->get_Tile_size().y >= mouse_position.y); });
+		if (it != board.end()) {
+
+
+
+
+		}
+		else {
+			setPosition(get_Starting_Piece_pos());
+		}
+
+	}
+}
 
 
 
