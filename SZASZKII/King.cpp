a2d44e28b7b99cfdb70f8f3c334964511907e184
LambuiB080 <lamblazej@gmail.com>
2023-06-11 16:14:05 +0200

    działające kolizje i bicie króla (bez szachowania itp.)

--- SZASZKII/King.cpp
+++ SZASZKII/King.cpp
@@ -21,7 +21,7 @@ void::King::move(std::vector<BoardTile*>& board, const sf::Vector2i& mouse_posit
 				Tile->get_Tile_position().y <= mouse_position.y && Tile->get_Tile_position().y + Tile->get_Tile_size().y >= mouse_position.y);
 			});
 
-		if (it != board.end() && (
+		if (it != board.end() && collider(_PawnsVec, (*it)->get_Tile_position()) && (
 			(*it)->get_Tile_position() == get_Starting_Piece_pos() + sf::Vector2f(1 * 112, 1 * 112) ||
 			(*it)->get_Tile_position() == get_Starting_Piece_pos() + sf::Vector2f(-1 * 112, 1 * 112) ||
 			(*it)->get_Tile_position() == get_Starting_Piece_pos() + sf::Vector2f(-1 * 112, -1 * 112) ||
@@ -37,5 +37,38 @@ void::King::move(std::vector<BoardTile*>& board, const sf::Vector2i& mouse_posit
 		else {
 			setPosition(get_Starting_Piece_pos());
 		}
+	}
+}
+
+bool King::collider(std::vector<Piece*> _PawnsVec, sf::Vector2f selected_Tile_pos) {
+	auto itr = std::find_if(_PawnsVec.begin(), _PawnsVec.end(), [selected_Tile_pos](Piece* _piece) {
+		return selected_Tile_pos == _piece->getPosition();
+		});
+	if (itr != _PawnsVec.end()) {
+		if (get_Piece_color() == Black) {
+			if ((*itr)->get_Piece_color() == White) {
+				(*itr)->setPosition(0, 0);
+				(*itr)->scale(0, 0);
+				_PawnsVec.erase(itr);
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else if (get_Piece_color() == White) {
+			if ((*itr)->get_Piece_color() == Black) {
+				(*itr)->setPosition(0, 0);
+				(*itr)->scale(0, 0);
+				_PawnsVec.erase(itr);
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	else {
+		return true;
 	}
 }
--- SZASZKII/King.h
+++ SZASZKII/King.h
@@ -8,4 +8,5 @@ private:
 public:
 	King(const std::string& _id, const Piece_colors _color, std::vector<BoardTile*>& board);
 	void move(std::vector<BoardTile*>& board, const sf::Vector2i& mouse_position, std::vector<Piece*> _PawnsVec);
+	bool collider(std::vector<Piece*> _PawnsVec, sf::Vector2f selected_Tile_pos);
 };
